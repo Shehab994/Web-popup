3e9bd0cb5eb29e3bb6d6f5f6116ef18253544cd3
shekh_x <shekh_x@Shehab.shekh_x>
2024-12-25 08:24:50 -0500

    added all file

---
+++ script.js
@@ -0,0 +1,15 @@
+
+
+
+
+
+let popup = document.getElementById('popup');
+
+
+		function openPopup(){
+			popup.classList.add("oprn-popup");
+		}
+
+		function closePopup(){
+			popup.classList.remove("oprn-popup");
+		}
